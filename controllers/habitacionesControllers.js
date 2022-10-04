760cb414053266014785f38d06ba7eea50f784ae
Ariok-David-LM <ariokdavidl@gmail.com>
2022-10-03 23:13:26 -0500

    Feat: CRUD para la tabla Habitacion con sus vistas

--- controllers/habitacionesControllers.js
+++ controllers/habitacionesControllers.js
@@ -4,32 +4,32 @@ import { Hoteles } from '../models/Hoteles.js'
 let habitacionUpdateId = ''
 
 // Renderizar Formulario para crear una Habitacion
-/*
 const paginaCreateHabitacion = async (req, res) => {
+  let hotelesModificados = []
   const hoteles = await Hoteles.findAll({
     attributes: ['id_htl', 'nombre']
   })
-  const habitaciones = await Habitaciones.findAll({
-    attributes: ['id_hotel', 'piso', 'nombre', 'refrigerador']
+  const hotelesModificados1 = JSON.parse(JSON.stringify(hoteles))
+  hotelesModificados1.map(hm1 =>{
+    let obj = {
+      id: hm1.id_htl,
+      nombre: hm1.nombre
+    }
+    hotelesModificados.push(obj)
   })
   res.render('formCHabitacion', {
     pagina: 'Añadir Habitacion',
-    hoteles: hoteles,
-    habitaciones: habitaciones
-  })
-}
-*/
-const paginaCreateHabitacion = (req, res) => {
-  res.render('formCHabitacion', {
-    pagina: 'Añadir Habitacion',
-    
+    hoteles: hotelesModificados
   })
 }
 
 // Enviar la nueva habitacion a la Base de Datos
-/*
 const createHabitacion = async (req, res) => {
-  const { id_hotel, piso, nombre, refrigerador } = req.body
+  const { id_hotel, piso, nombre } = req.body
+  let refrigerador = false
+  if (req.body?.refrigerador) {
+    refrigerador = true
+  }
   const errores = []
 
   if (id_hotel.trim() === '') {
@@ -41,10 +41,6 @@ const createHabitacion = async (req, res) => {
   if (nombre.trim() === '') {
     errores.push({ mensaje: 'El nombre no puede quedar vacio' })
   }
-  if (refrigerador.trim() === '') {
-    errores.push({ mensaje: 'Selecciona un valor de refrigerador' })
-  }
-
   if (errores.length > 0) {
     res.render('formCHabitacion', {
       pagina: 'Añadir Habitacion',
@@ -57,67 +53,112 @@ const createHabitacion = async (req, res) => {
   } else {
     // Almacenar en la base de datos
     try {
-      await Hoteles.create({
+      await Habitaciones.create({
         id_hotel,
         piso,
         nombre,
         refrigerador
-      })
+      }, { fields: ['id_hotel', 'piso', 'nombre', 'refrigerador'] })
       res.redirect('/habitaciones')
     } catch (error) {
       console.log(error)
     }
   }
 }
-*/
-const createHabitacion = (req, res) => {
-  res.redirect('/habitaciones')
-}
 
 // Renderizar pagina de los Habitaciones
 const paginaReadHabitaciones = async (req, res) => {
+  let hotelesTotales = []
   const hoteles = await Hoteles.findAll({
-    attributes: ['nombre']
+    attributes: ['id_htl', 'nombre']
   })
+  const hotelesTotales1 = JSON.parse(JSON.stringify(hoteles))
+  hotelesTotales1.map(htls => {
+    let obj ={
+      id: htls.id_htl,
+      nombre: htls.nombre
+    }
+    hotelesTotales.push(obj)
+  })
+  let habitacionesTotales = []
   const habitaciones = await Habitaciones.findAll({
-    attributes: ['id_hotel', 'piso', 'nombre', 'refrigerador']
+    attributes: ['id_hbt', 'id_hotel', 'piso', 'nombre', 'refrigerador']
+  })
+  const habitacionesTotales1 = JSON.parse(JSON.stringify(habitaciones))
+  habitacionesTotales1.map(ht1 => {
+    let hotelObj = {}
+    hotelesTotales.map(ht => {
+      if (ht.id === ht1.id_hotel) {
+        hotelObj = ht
+      }
+    })
+    const nombreHotel = hotelObj.nombre
+    let obj = {
+      id: ht1.id_hbt,
+      nombreHotel: nombreHotel,
+      nombre: ht1.nombre,
+      piso: ht1.piso,
+      refri: ht1.refrigerador
+    }
+    habitacionesTotales.push(obj)
   })
   res.render('habitaciones', {
     pagina: 'Habitaciones',
-    habitaciones: habitaciones,
-    hoteles: hoteles
+    habitaciones: habitacionesTotales,
   })
 }
 
 // Renderizar formulario para modificar habitacion
-/*
 const paginaUpdateHabitacion = async (req, res) => {
-  try {
-    const habitaciones = await Habitaciones.findByPk(req.query.id)
-    const hoteles = await Hoteles.findAll({
-      attributes: ['id_htl', 'nombre']
-    })
-    habitacionUpdateId = req.query.id
-    res.render('formUHabitaciones', {
-      pagina: 'Editar Habitacion',
-      hotel: hoteles,
-      habitaciones: habitaciones
-    })
-  } catch (error) {
-    console.log(error);
+  const habitaciones = await Habitaciones.findAll({
+    attributes: ['id_hbt', 'id_hotel', 'nombre', 'piso', 'refrigerador'],
+    where: {
+      id_hbt: req.query.id
+    }
+  })
+  habitacionUpdateId = req.query.id
+  const habitacion1 = JSON.parse(JSON.stringify(habitaciones))
+  let habitacionMOD = {
+    id: habitacion1[0].id_hbt,
+    hotelId: habitacion1[0].id_hotel,
+    nombre: habitacion1[0].nombre,
+    piso: habitacion1[0].piso,
+    refri: habitacion1[0].refrigerador
   }
-}
-*/
-const paginaUpdateHabitacion = (req, res) => {
+
+  var idPrueba = habitacionMOD.hotelId
+
+  let hotelesMOd = []
+  const hoteles = await Hoteles.findAll({
+    attributes: ['id_htl', 'nombre']
+  })
+  const hoteles1 = JSON.parse(JSON.stringify(hoteles))
+  hoteles1.map(ht1 => {
+    let selectedHTL = false
+    if (ht1.id_htl === idPrueba) {
+      selectedHTL = true
+    }
+    let obj = {
+      id: ht1.id_htl,
+      nombre: ht1.nombre,
+      opcion: selectedHTL
+    }
+    hotelesMOd.push(obj)
+  })
   res.render('formUHabitaciones', {
     pagina: 'Editar Habitacion',
-   
+    hoteles: hotelesMOd,
+    habitacion: habitacionMOD
   })
 }
+
 // Enviar el habitacion actualizado a la base de datos
-/*
 const updateHabitacion = async (req, res) => {
-  const { id_hotel, piso, nombre, refrigerador } = req.body
+  const { id_hotel, piso, nombre } = req.body
+  let refrigerador = false
+  if (req.body?.refrigerador) {
+    refrigerador = true
+  }
   const errores = []
 
   if (id_hotel.trim() === '') {
@@ -129,10 +170,6 @@ const updateHabitacion = async (req, res) => {
   if (nombre.trim() === '') {
     errores.push({ mensaje: 'El nombre no puede quedar vacio' })
   }
-  if (refrigerador.trim() === '') {
-    errores.push({ mensaje: 'Selecciona un valor de refrigerador' })
-  }
-
   if (errores.length > 0) {
     res.render('formCHabitacion', {
       pagina: 'Añadir Habitacion',
@@ -145,14 +182,14 @@ const updateHabitacion = async (req, res) => {
   } else {
     // Almacenar en la base de datos
     try {
-      await Hoteles.update({
+      await Habitaciones.update({
         id_hotel,
         piso,
         nombre,
         refrigerador
       }, {
         where: {
-          id: habitacionUpdateId
+          id_hbt: habitacionUpdateId
         }
       })
       res.redirect('/habitaciones')
@@ -161,17 +198,13 @@ const updateHabitacion = async (req, res) => {
     }
   }
 }
-*/
-const updateHabitacion = (req, res) => {
-  res.redirect('/habitaciones')
-}
 
 // Eliminar gerente
 const paginaDeleteHabitaciones = async (req, res) => {
   try {
     await Habitaciones.destroy({
       where: {
-        id: req.query.id
+        id_hbt: req.query.id
       }
     })
     res.redirect('/habitaciones')
