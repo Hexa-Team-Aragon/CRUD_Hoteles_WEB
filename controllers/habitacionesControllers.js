02ba3150af46be0ea51f9fca9695dd9d702d95a0
axelnerilucero <neriluceroaxel@gmail.com>
2022-11-15 06:34:46 -0600

    Feat: Validacion del create habitaciones 2

--- controllers/habitacionesControllers.js
+++ controllers/habitacionesControllers.js
@@ -39,51 +39,7 @@ const createHabitacion = async (req, res) => {
   } catch (error) {
     console.log(error)
   }
-}/*let refrigerador = false
-  if (req.body?.refrigerador) {
-    refrigerador = true
-  }
-  const errores = []
-
-  if (id_hotel === '0') {
-    errores.push({ mensaje: 'Selecciona un hotel' })
-  }
-  if (piso.trim() === '') {
-    errores.push({ mensaje: 'El piso no puede quedar vacio' })
-  }
-  if (nombre.trim() === '') {
-    errores.push({ mensaje: 'El nombre no puede quedar vacio' })
-  }
-  if (errores.length > 0) {
-    let hotelesModificados = []
-    const hoteles = await Hoteles.findAll({
-      attributes: ['id_htl', 'nombre']
-    })
-    const hotelesModificados1 = JSON.parse(JSON.stringify(hoteles))
-    hotelesModificados1.map(hm1 => {
-      let selec = false
-      if (hm1.id_htl == id_hotel) {
-        selec = true
-      }
-      let obj = {
-        id: hm1.id_htl,
-        nombre: hm1.nombre,
-        selected: selec
-      }
-      hotelesModificados.push(obj)
-    })
-    res.render('formCHabitacion', {
-      pagina: 'Añadir Habitacion',
-      errores,
-      hoteles: hotelesModificados,
-      id_hotel, 
-      piso, 
-      nombre,
-      refrigerador
-    })
-  } else {
-    */ // Almacenar en la base de datos
-
+}
 
 // Renderizar pagina de los Habitaciones
 const paginaReadHabitaciones = async (req, res) => {
